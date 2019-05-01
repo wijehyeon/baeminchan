bbbaccfd8bc33f0df481919054e2271c88efb03e
wijehyeon <xile803@gmail.com>
2019-05-01 17:40:12 +0900

    add : gitignore refresh

--- target/classes/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO USER (id, email, name, password , phone_number) VALUES (1, 't', 'jehyeon', 't', '010-1234-1234');
-INSERT INTO USER (id, email, name, password , phone_number) VALUES (2, 'email@email.com', 'jehyeon', 'password', '010-1234-1234');
