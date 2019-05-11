8488c1934a1302a0ee9c84f896e905ad657db509
wijehyeon <xile803@gmail.com>
2019-05-11 14:34:41 +0900

    refactor : MvcConfig annotation 추가

--- src/main/java/codesquad/config/MvcConfig.java
+++ src/main/java/codesquad/config/MvcConfig.java
@@ -1,14 +1,15 @@
 package codesquad.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        registry.addViewController("/users/form").setViewName("users/form");
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/users/join").setViewName("join");
         registry.addViewController("/users/loginForm").setViewName("login");
